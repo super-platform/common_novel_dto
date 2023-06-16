3f207542b7c02b311a875517b13ed37ea52673c5
tutruongmugi <khongthenho304@gmail.com>
2023-06-16 13:58:19 +0700

    add jwtHelper

--- src/main/java/com/ngtu/api/common/utils/JwtHelper.java
+++ src/main/java/com/ngtu/api/common/utils/JwtHelper.java
@@ -1,5 +1,18 @@
 package com.ngtu.api.common.utils;
 
-public class JwtHelper {
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.Claim;
 
+public class JwtHelper {
+    public static String getUsername(String bearerToken){
+        DecodedJWT jwt = JWT.decode(getJWTToken(bearerToken));
+        Claim emailClaim = jwt.getClaims().get("preferred_username");
+        return emailClaim != null ? emailClaim.asString() : null;
+    }
+
+    public static String getJWTToken(String bearerToken){
+        String jwtToken = bearerToken.replace("Bearer ","");
+        return jwtToken;
+    }
 }
